729ca62ae1e1e1eea5b3ed6e88f476153a8e759d
uolter <walter.traspadini@pagopa.it>
2021-05-13 14:52:00 +0200

    vent module address space output

--- virtual_network/outputs.tf
+++ virtual_network/outputs.tf
@@ -9,3 +9,7 @@ output "name" {
 output "resource_group_name" {
   value = azurerm_virtual_network.this.resource_group_name
 }
+
+output "address_space" {
+  value = azurerm_virtual_network.this.address_space
+}
