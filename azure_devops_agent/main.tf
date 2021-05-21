8b9e031ff29cddf21293f507e34adf04337446f5
pasqualedevita <>
2021-05-21 16:56:38 +0200

    add scale set with

---
+++ azure_devops_agent/main.tf
@@ -0,0 +1,52 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "=3.1.0"
+    }
+  }
+}
+
+resource "null_resource" "this" {
+  # needs az cli > 2.0.81
+  # see https://github.com/Azure/azure-cli/issues/12152
+
+  triggers = {
+    name                = var.name
+    resource_group_name = var.resource_group_name
+    subscription        = var.subscription
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+      az login --service-principal --username $ARM_CLIENT_ID --password $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID && \
+      az account set -s ${var.subscription} && \
+      az vmss create \
+        --name ${var.name} \
+        --resource-group ${var.resource_group_name} \
+        --image ${var.image} \
+        --vm-sku ${var.vm_sku} \
+        --storage-sku ${var.storage_sku} \
+        --authentication-type ${var.authentication_type} \
+        --generate-ssh-keys \
+        --instance-count 2 \
+        --disable-overprovision \
+        --upgrade-policy-mode manual \
+        --single-placement-group false \
+        --platform-fault-domain-count 1 \
+        --load-balancer "" \
+        --subnet ${var.subnet_id}
+    EOT
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = <<EOT
+      az login --service-principal --username $ARM_CLIENT_ID --password $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID && \
+      az account set -s ${self.triggers.subscription} && \
+      az vmss delete \
+        --name ${self.triggers.name} \
+        --resource-group ${self.triggers.resource_group_name}
+    EOT
+  }
+}
--- virtual_machine_scaleset_linux/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "id" {
-  value = azurerm_linux_virtual_machine_scale_set.this.id
-}
-
-output "name" {
-  value = azurerm_linux_virtual_machine_scale_set.this.name
-}
