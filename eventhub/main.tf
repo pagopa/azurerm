feefb01a9fc5bef8d5d5f9042de87f68831e5894
uolter <walter.traspadini@pagopa.it>
2021-05-27 15:51:55 +0200

    module for event hub and eventhub namespace.

---
+++ eventhub/main.tf
@@ -0,0 +1,54 @@
+resource "azurerm_eventhub_namespace" "this" {
+  name                     = var.name
+  location                 = var.location
+  resource_group_name      = var.resource_group_name
+  sku                      = var.sku
+  capacity                 = var.capacity
+  auto_inflate_enabled     = var.auto_inflate_enabled
+  maximum_throughput_units = var.auto_inflate_enabled ? var.maximum_throughput_units : null
+  zone_redundant           = var.zone_redundant
+
+  dynamic "network_rulesets" {
+    for_each = var.network_rulesets
+    content {
+      default_action = network_rulesets.value["default_action"]
+      # virtual_network_rule {} # optional one ore more
+      dynamic "virtual_network_rule" {
+        for_each = network_rulesets.value["virtual_network_rule"]
+        content {
+          subnet_id                                       = virtual_network_rule.value["subnet_id"]
+          ignore_missing_virtual_network_service_endpoint = virtual_network_rule.value["ignore_missing_virtual_network_service_endpoint"]
+        }
+      }
+      dynamic "ip_rule" {
+        for_each = network_rulesets.value["ip_rule"]
+        content {
+          ip_mask = ip_rule.value["ip_mask"]
+          action  = ip_rule.value["action"]
+        }
+      }
+    }
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_eventhub" "eventhub" {
+  count               = length(var.eventhubs)
+  name                = var.eventhubs[count.index].name
+  namespace_name      = azurerm_eventhub_namespace.this.name
+  resource_group_name = var.resource_group_name
+  partition_count     = var.eventhubs[count.index].partition_count
+  message_retention   = var.eventhubs[count.index].message_retention
+}
+
+resource "azurerm_eventhub_authorization_rule" "eventhub_rule" {
+  count               = length(var.eventhub_authorization_rules)
+  name                = var.eventhub_authorization_rules[count.index].name
+  namespace_name      = azurerm_eventhub_namespace.this.name
+  resource_group_name = var.resource_group_name
+  eventhub_name       = var.eventhub_authorization_rules[count.index].eventhub_name
+  listen              = var.eventhub_authorization_rules[count.index].listen
+  send                = var.eventhub_authorization_rules[count.index].send
+  manage              = var.eventhub_authorization_rules[count.index].manage
+}
---
+++ eventhub/variables.tf
@@ -0,0 +1,75 @@
+variable "name" {
+  type        = string
+  description = "Eventhub namespace description."
+}
+
+variable "location" {
+  type = string
+}
+
+// Resource Group 
+variable "resource_group_name" {
+  type = string
+}
+
+variable "auto_inflate_enabled" {
+  type        = bool
+  description = "Is Auto Inflate enabled for the EventHub Namespace?"
+  default     = false
+}
+
+variable "eventhubs" {
+  type = list(object({
+    name              = string
+    partition_count   = number
+    message_retention = number
+  }))
+}
+
+variable "sku" {
+  type        = string
+  description = "Defines which tier to use. Valid options are Basic and Standard."
+}
+
+variable "capacity" {
+  type        = number
+  description = "Specifies the Capacity / Throughput Units for a Standard SKU namespace."
+  default     = null
+}
+
+variable "maximum_throughput_units" {
+  type        = number
+  description = "Specifies the maximum number of throughput units when Auto Inflate is Enabled"
+  default     = null
+}
+
+variable "network_rulesets" {
+  type = list(object({
+    default_action = string
+    virtual_network_rule = list(object({
+      subnet_id                                       = string
+      ignore_missing_virtual_network_service_endpoint = bool
+    }))
+    ip_rule = list(object({
+      ip_mask = string
+      action  = string
+    }))
+  }))
+  default = []
+}
+
+variable "eventhub_authorization_rules" {
+  description = "The list of eventhub authorization rules. Each rule is expressed as a dictionary. Each dictionary can contain the keys name, eventhub_name ,listen, send, manage"
+  type        = list(any)
+  default     = []
+}
+
+variable "zone_redundant" {
+  type        = bool
+  default     = false
+  description = "Specifies if the EventHub Namespace should be Zone Redundant (created across Availability Zones)."
+}
+
+variable "tags" {
+  type = map(any)
+}
