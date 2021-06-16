9e33a6641f411d8c1bebbdb83057ec479a5221a8
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2021-06-16 17:55:24 +0200

    Add app service module (#32)
    Co-authored-by: pasqualedevita <>

---
+++ app_service/main.tf
@@ -0,0 +1,82 @@
+resource "azurerm_app_service_plan" "this" {
+  name                = var.plan_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  kind = var.plan_kind
+
+  sku {
+    tier     = var.plan_sku_tier
+    size     = var.plan_sku_size
+    capacity = var.plan_sku_capacity
+  }
+
+  maximum_elastic_worker_count = var.plan_maximum_elastic_worker_count
+  reserved                     = var.plan_reserved
+  per_site_scaling             = var.plan_per_site_scaling
+
+  tags = var.tags
+}
+
+resource "azurerm_app_service" "this" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  app_service_plan_id = azurerm_app_service_plan.this.id
+  https_only          = true
+  client_cert_enabled = var.client_cert_enabled
+
+  app_settings = var.app_settings
+
+  site_config {
+    always_on        = var.always_on
+    linux_fx_version = var.linux_fx_version
+    app_command_line = var.app_command_line
+    min_tls_version  = "1.2"
+    ftps_state       = "Disabled"
+
+    health_check_path = var.health_check_path != null ? var.health_check_path : null
+
+    dynamic "ip_restriction" {
+      for_each = var.allowed_subnets
+      iterator = subnet
+
+      content {
+        ip_address                = null
+        virtual_network_subnet_id = subnet.value
+      }
+    }
+
+    dynamic "ip_restriction" {
+      for_each = var.allowed_ips
+      iterator = ip
+
+      content {
+        ip_address                = ip.value
+        virtual_network_subnet_id = null
+      }
+    }
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = var.tags
+
+  lifecycle {
+    ignore_changes = [
+      site_config.0.scm_type,
+      site_config.0.linux_fx_version, # deployments are made outside of Terraform
+      app_settings["DOCKER_CUSTOM_IMAGE_NAME"]
+    ]
+  }
+}
+
+resource "azurerm_app_service_virtual_network_swift_connection" "app_service_virtual_network_swift_connection" {
+  count = var.subnet_name != null ? 1 : 0
+
+  app_service_id = azurerm_app_service.this.id
+  subnet_id      = var.subnet_id
+}
