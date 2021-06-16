5ddfdad803ce2c6efd14b00e6726a40688f587e1
Matteo Gazzetta <matteo.gazzetta@bitrock.it>
2021-06-16 16:35:09 +0200

    Add optional replica to postgresql module

--- postgresql_server/main.tf
+++ postgresql_server/main.tf
@@ -22,6 +22,34 @@ resource "azurerm_postgresql_server" "this" {
   tags = var.tags
 }
 
+resource "azurerm_postgresql_server" "replica" {
+  count               = var.enable_replica ? 1 : 0
+  name                = format("%s-rep", var.name)
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  administrator_login          = var.administrator_login
+  administrator_login_password = var.administrator_login_password
+
+  sku_name   = var.sku_name
+  version    = var.db_version
+  storage_mb = var.storage_mb
+
+  backup_retention_days        = var.backup_retention_days
+  geo_redundant_backup_enabled = var.geo_redundant_backup_enabled
+
+  auto_grow_enabled = var.auto_grow_enabled
+
+  public_network_access_enabled    = var.public_network_access_enabled
+  ssl_enforcement_enabled          = var.ssl_enforcement_enabled
+  ssl_minimal_tls_version_enforced = var.ssl_minimal_tls_version_enforced
+
+  create_mode               = var.create_mode
+  creation_source_server_id = azurerm_postgresql_server.this.id
+
+  tags = var.tags
+}
+
 resource "azurerm_private_dns_zone" "private_dns_zone_postgres" {
   name                = "privatelink.postgres.database.azure.com"
   resource_group_name = var.resource_group_name
