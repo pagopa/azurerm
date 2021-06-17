4e9f6b00b7e9828d3c39053054121cb12cfaf146
Matteo Gazzetta <matteo_gazzetta@hotmail.it>
2021-06-17 11:04:14 +0200

    Add apim product module (#34)

---
+++ api_management_product/variables.tf
@@ -0,0 +1,51 @@
+variable "product_id" {
+  type        = string
+  description = "The Identifier for this Product, which must be unique within the API Management Service."
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "The name of the Resource Group in which the API Management Service should be exist."
+}
+
+variable "api_management_name" {
+  type        = string
+  description = "The name of the API Management Service."
+}
+
+variable "display_name" {
+  type        = string
+  description = "The Display Name for this API Management Product."
+}
+
+variable "description" {
+  type        = string
+  description = "A description of this Product, which may include HTML formatting tags."
+}
+
+variable "subscription_required" {
+  type        = bool
+  description = "Is a Subscription required to access API's included in this Product?"
+}
+
+variable "approval_required" {
+  type        = bool
+  description = "Do subscribers need to be approved prior to being able to use the Product?"
+}
+
+variable "published" {
+  type        = bool
+  description = "Is this Product Published?"
+}
+
+variable "policy_xml" {
+  type        = string
+  description = "The XML Content for this Product Policy."
+  default     = null
+}
+
+variable "groups" {
+  type        = list(string)
+  description = "The groups that the product is included"
+  default     = []
+}
