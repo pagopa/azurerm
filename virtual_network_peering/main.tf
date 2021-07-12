daf95c32ad6ea15b09e89d98ee700761ac3b1fa5
uolter <walter.traspadini@pagopa.it>
2021-07-12 12:13:39 +0200

    virtual network peering module

---
+++ virtual_network_peering/main.tf
@@ -0,0 +1,23 @@
+resource "azurerm_virtual_network_peering" "source" {
+  name                      = format("%s-to-%s", var.source_virtual_network_name, var.target_virtual_network_name)
+  resource_group_name       = var.source_resource_group_name
+  virtual_network_name      = var.source_virtual_network_name
+  remote_virtual_network_id = var.target_remote_virtual_network_id
+
+  allow_virtual_network_access = var.source_allow_virtual_network_access
+  allow_forwarded_traffic      = var.source_allow_forwarded_traffic
+  allow_gateway_transit        = var.source_allow_gateway_transit
+  use_remote_gateways          = var.source_use_remote_gateways
+}
+
+resource "azurerm_virtual_network_peering" "target" {
+  name                      = format("%s-to-%s", var.source_virtual_network_name, var.target_virtual_network_name)
+  resource_group_name       = var.target_resource_group_name
+  virtual_network_name      = var.target_virtual_network_name
+  remote_virtual_network_id = var.source_remote_virtual_network_id
+
+  allow_virtual_network_access = var.target_allow_virtual_network_access
+  allow_forwarded_traffic      = var.target_allow_forwarded_traffic
+  allow_gateway_transit        = var.target_allow_gateway_transit
+  use_remote_gateways          = var.target_use_remote_gateways
+}
---
+++ virtual_network_peering/variables.tf
@@ -0,0 +1,83 @@
+variable "location" {
+  type = string
+}
+
+variable "source_resource_group_name" {
+  type        = string
+  description = "The name of the resource group in which to start the virtual network peering"
+}
+
+variable "target_resource_group_name" {
+  type        = string
+  description = "The name of the resource group in which to end the virtual network peering"
+  default     = null
+}
+
+variable "source_virtual_network_name" {
+  type        = string
+  description = "The name of the virtual network from which the peering starts"
+}
+
+variable "target_virtual_network_name" {
+  type        = string
+  description = "The name of the virtual network from which the peering ends"
+}
+
+variable "source_remote_virtual_network_id" {
+  type        = string
+  description = "The full Azure resource ID of the remote virtual network from which the peering starts."
+}
+
+variable "target_remote_virtual_network_id" {
+  type        = string
+  description = "The full Azure resource ID of the remote virtual network from which the peering ends."
+}
+
+variable "source_allow_virtual_network_access" {
+  type        = bool
+  description = "Controls if the VMs in the remote virtual network can access VMs in the local virtual network."
+  default     = true
+}
+
+variable "target_allow_virtual_network_access" {
+  type        = bool
+  description = "Controls if the VMs in the remote virtual network can access VMs in the local virtual network."
+  default     = true
+}
+
+variable "source_allow_forwarded_traffic" {
+  type        = bool
+  description = "Controls if forwarded traffic from VMs in the remote virtual network is allowed."
+  default     = false
+}
+
+variable "target_allow_forwarded_traffic" {
+  type        = bool
+  description = "Controls if forwarded traffic from VMs in the remote virtual network is allowed."
+  default     = false
+}
+
+variable "source_allow_gateway_transit" {
+  type        = bool
+  description = "Controls gatewayLinks can be used in the remote virtual network’s link to the local virtual network."
+  default     = false
+}
+
+variable "target_allow_gateway_transit" {
+  type        = bool
+  description = "Controls gatewayLinks can be used in the remote virtual network’s link to the local virtual network."
+  default     = false
+}
+
+variable "source_use_remote_gateways" {
+  type        = bool
+  description = "Controls if remote gateways can be used on the local virtual network"
+  default     = false
+}
+
+variable "target_use_remote_gateways" {
+  type        = bool
+  description = "Controls if remote gateways can be used on the local virtual network"
+  default     = false
+}
+
