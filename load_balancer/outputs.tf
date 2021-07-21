29ebb190b2f33c6d1f0ce0ee0ae5d23dc2e2f991
Matteo Gazzetta <matteo.gazzetta@bitrock.it>
2021-07-21 21:52:03 +0200

    Remove not needed outputs

--- load_balancer/outputs.tf
+++ load_balancer/outputs.tf
@@ -8,11 +8,6 @@ output "azurerm_lb_frontend_ip_configuration" {
   value       = azurerm_lb.this.frontend_ip_configuration
 }
 
-output "azurerm_lb_probe_ids" {
-  description = "the ids for the azurerm_lb_probe resources"
-  value       = azurerm_lb_probe.this.*.id
-}
-
 output "azurerm_public_ip_id" {
   description = "the id for the azurerm_lb_public_ip resource"
   value       = azurerm_public_ip.this.*.id
@@ -23,7 +18,3 @@ output "azurerm_public_ip_address" {
   value       = azurerm_public_ip.this.*.ip_address
 }
 
-output "azurerm_lb_backend_address_pool_id" {
-  description = "the id for the azurerm_lb_backend_address_pool resources"
-  value       = azurerm_lb_backend_address_pool.this.*.id
-}
