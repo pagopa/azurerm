4feed06b0c8533ec400f7b184e1e922d929e2e06
pasqualedevita <>
2021-09-24 13:21:01 +0200

    test

--- kubernetes_cluster/main.tf
+++ kubernetes_cluster/main.tf
@@ -18,11 +18,11 @@ resource "azurerm_kubernetes_cluster" "this" {
     min_count           = var.min_count
     max_count           = var.max_count
 
-    upgrade_settings {
-      max_surge = var.upgrade_settings_max_surge
-    }
-
     tags = var.tags
+  }
+
+  upgrade_settings {
+    max_surge = var.upgrade_settings_max_surge
   }
 
   automatic_channel_upgrade       = var.automatic_channel_upgrade
