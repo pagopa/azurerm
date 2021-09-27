f754f1b5bdfa01f433ac45c54c4a35b5bbed1d51
pasqualedevita <>
2021-09-27 19:09:09 +0200

    add vmss extension for az cli

---
+++ azure_devops_agent/script-config.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# install az cli from https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-linux?pivots=apt#option-2-step-by-step-installation-instructions
+
+apt-get -y update
+apt-get -y install ca-certificates curl apt-transport-https lsb-release gnupg
+
+curl -sL https://packages.microsoft.com/keys/microsoft.asc |
+    gpg --dearmor |
+    tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
+    tee /etc/apt/sources.list.d/azure-cli.list
+
+apt-get -y update
+apt-get -y install azure-cli
