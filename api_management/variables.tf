22467ba6153557a7fbeb2ad1b42e10967964d3c7
giacut <88658486+giacut@users.noreply.github.com>
2021-10-01 15:04:50 +0200

    Dev diagnosticsettings (#83)
    * adding diagnostic settings in api_management module
    
    * adding diagnostic settings in api_management module
    
    Co-authored-by: nowayout <spartak@autistici.org>
    Co-authored-by: Gianpietro.Cutolo <gianpietro.cutolo@pagopa.it>

--- api_management/variables.tf
+++ api_management/variables.tf
@@ -258,6 +258,18 @@ variable "lock_enable" {
   description = "Apply lock to block accedentaly deletions."
 }
 
+variable "sec_log_analytics_workspace_id" {
+  type        = string
+  default     = null
+  description = "Log analytics workspace security (it should be in a different subscription)."
+}
+
+variable "sec_storage_id" {
+  type        = string
+  default     = null
+  description = "Storage Account security (it should be in a different subscription)."
+}
+
 variable "tags" {
   type = map(any)
 }
