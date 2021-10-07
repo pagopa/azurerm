d2564a17162c225823d31ec50dc7319946cf551e
pasqualedevita <>
2021-10-07 17:26:15 +0200

    add connection string to output Co-authored-by: fabriziopapi <fabriziopapi@users.noreply.github.com>

--- eventhub/outputs.tf
+++ eventhub/outputs.tf
@@ -17,8 +17,10 @@ output "keys" {
   description = "Map of hubs with keys => primary_key / secondary_key mapping."
   sensitive   = true
   value = { for k, h in azurerm_eventhub_authorization_rule.events : k => {
-    primary_key   = h.primary_key
-    secondary_key = h.secondary_key
+    primary_key                 = h.primary_key
+    primary_connection_string   = h.primary_connection_string
+    secondary_key               = h.secondary_key
+    secondary_connection_string = h.secondary_connection_string
     }
   }
 }
