4f4efe5656bbd80f0606df58a79f4cbeee784d41
uolter <walter.traspadini@pagopa.it>
2021-11-08 15:44:09 +0100

    output load balancer private ip + fix deprecated variable

--- load_balancer/outputs.tf
+++ load_balancer/outputs.tf
@@ -13,8 +13,12 @@ output "azurerm_public_ip_id" {
   value       = azurerm_public_ip.this.*.id
 }
 
+output "azurerm_private_ip_address" {
+  description = "Private IP Address to assign to the Load Balancer."
+  value       = azurerm_lb.this.private_ip_address
+}
+
 output "azurerm_public_ip_address" {
   description = "the ip address for the azurerm_lb_public_ip resource"
   value       = azurerm_public_ip.this.*.ip_address
 }
-
