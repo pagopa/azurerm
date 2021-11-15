f30abea2cfa094e66ae8077f745d5bcd4dda0cec
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2021-11-15 21:36:38 +0100

    exporting storage infos (#115)
    Co-authored-by: anttorre <antonio.torre@everis.com>

--- cdn/outputs.tf
+++ cdn/outputs.tf
@@ -6,6 +6,29 @@ output "hostname" {
   value = "${azurerm_cdn_endpoint.this.name}.azureedge.net"
 }
 
+output "storage_primary_connection_string" {
+  value     = module.cdn_storage_account.primary_connection_string
+  sensitive = true
+}
+
+output "storage_primary_access_key" {
+  value     = module.cdn_storage_account.primary_access_key
+  sensitive = true
+}
+
+output "storage_primary_blob_connection_string" {
+  value     = module.cdn_storage_account.primary_blob_connection_string
+  sensitive = true
+}
+
+output "storage_primary_blob_host" {
+  value = module.cdn_storage_account.primary_blob_host
+}
+
+output "storage_primary_web_host" {
+  value = module.cdn_storage_account.primary_web_host
+}
+
 output "name" {
   value = azurerm_cdn_endpoint.this.name
 }
