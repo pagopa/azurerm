0b3641c5d5e34b2df10864f0f28cff02ebe9ad70
uolter <walter.traspadini@pagopa.it>
2021-11-24 15:27:59 +0100

    first draft module storage object replication

---
+++ storage_object_replication/main.tf
@@ -0,0 +1,17 @@
+/*
+Storage account object replication.
+*/
+
+resource "azurerm_storage_object_replication" "this" {
+  source_storage_account_id      = var.source_storage_account_id
+  destination_storage_account_id = var.destination_storage_account_id
+
+  dynamic "rules" {
+    for_each = var.rules
+    iterator = r
+    content {
+      source_container_name      = r.value.source_container_name
+      destination_container_name = r.value.destination_container_name
+    }
+  }
+}
---
+++ storage_object_replication/variables.tf
@@ -0,0 +1,21 @@
+variable "source_storage_account_id" {
+  type        = string
+  description = "The ID of the source storage account."
+}
+
+variable "destination_storage_account_id" {
+  type        = string
+  description = "The ID of the destination storage account."
+}
+
+variable "rules" {
+  type = list(
+    object({
+      source_container_name        = string
+      destination_container_name   = string
+      copy_blobs_created_after     = string
+      filter_out_blobs_with_prefix = string
+    })
+  )
+
+}
