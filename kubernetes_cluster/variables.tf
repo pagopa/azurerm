6c15d70777cb3f3e1ee60ada6c9b5459e05a2a99
Gianpietro.Cutolo <gianpietro.cutolo@pagopa.it>
2021-12-09 12:32:22 +0100

    enable aks audit logs - add variables

--- kubernetes_cluster/variables.tf
+++ kubernetes_cluster/variables.tf
@@ -204,6 +204,18 @@ variable "alerts_enabled" {
   description = "Should Metrics Alert be enabled?"
 }
 
+variable "sec_log_analytics_workspace_id" {
+  type        = string
+  default     = null
+  description = "Log analytics workspace security (it should be in a different subscription)."
+}
+
+variable "sec_storage_id" {
+  type        = string
+  default     = null
+  description = "Storage Account security (it should be in a different subscription)."
+}
+
 variable "tags" {
   type = map(any)
 }
