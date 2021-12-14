fd3647fb5209fac4a9b8bacb6b646e0426e9e92d
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2021-12-14 17:13:39 +0100

    add private endpoint (#137)
    Co-authored-by: pasqualedevita <>

--- redis_cache/main.tf
+++ redis_cache/main.tf
@@ -1,16 +1,16 @@
 resource "azurerm_redis_cache" "this" {
-  name                      = var.name
-  location                  = var.location
-  resource_group_name       = var.resource_group_name
-  capacity                  = var.capacity
-  shard_count               = var.shard_count
-  enable_non_ssl_port       = var.enable_non_ssl_port
-  minimum_tls_version       = "1.2"
-  subnet_id                 = var.subnet_id
-  private_static_ip_address = var.private_static_ip_address
-  family                    = var.family
-  sku_name                  = var.sku_name
-
+  name                          = var.name
+  location                      = var.location
+  resource_group_name           = var.resource_group_name
+  capacity                      = var.capacity
+  shard_count                   = var.shard_count
+  enable_non_ssl_port           = var.enable_non_ssl_port
+  minimum_tls_version           = "1.2"
+  subnet_id                     = var.subnet_id
+  private_static_ip_address     = var.private_static_ip_address
+  family                        = var.family
+  sku_name                      = var.sku_name
+  public_network_access_enabled = var.public_network_access_enabled
 
   redis_configuration {
     enable_authentication         = var.enable_authentication
@@ -37,4 +37,27 @@ resource "azurerm_redis_cache" "this" {
   lifecycle {
     ignore_changes = [redis_configuration[0].rdb_storage_connection_string]
   }
+}
+
+resource "azurerm_private_endpoint" "this" {
+  count = var.private_endpoint.enabled ? 1 : 0
+
+  name                = format("%s-private-endpoint", azurerm_redis_cache.this.name)
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.private_endpoint.subnet_id
+
+  private_dns_zone_group {
+    name                 = format("%s-private-dns-zone-group", azurerm_redis_cache.this.name)
+    private_dns_zone_ids = var.private_endpoint.private_dns_zone_ids
+  }
+
+  private_service_connection {
+    name                           = format("%s-private-service-connection", azurerm_redis_cache.this.name)
+    private_connection_resource_id = azurerm_redis_cache.this.id
+    is_manual_connection           = false
+    subresource_names              = ["redisCache"]
+  }
+
+  tags = var.tags
 }
