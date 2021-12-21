9a077ab9a8077444288651331109dbe752f52a53
Walter Traspadini <walter.traspadini@pagopa.it>
2021-12-21 16:57:29 +0100

    Application insight web test module (#140)
    * first draft module to set a web test checking the ssl crtificate expiry date
    
    * template param as local definition
    
    * fix local.name not declared.
    
    * moved template function in localblock
    
    * missed location paramenter
    
    * add etag value as required
    
    * removed etag property
    
    * missed api version in resource block
    
    * alert rule
    
    * fix typo in format function
    
    * fix web test id.
    
    * update metric alert scope
    
    * + severity alert
    
    * alerts depends on template resource.

---
+++ application_insights_web_test_preview/main.tf
@@ -0,0 +1,66 @@
+locals {
+  template_body = templatefile("${path.module}/template.tftpl", {
+    subscription_id                   = var.subscription_id
+    web_test_name                     = var.name
+    resource_group                    = var.resource_group
+    application_insight_name          = var.application_insight_name
+    frequency                         = var.frequency
+    timeout                           = var.timeout
+    request_url                       = var.request_url
+    expected_http_status              = var.expected_http_status
+    ignore_http_status                = var.ignore_http_status
+    ssl_cert_remaining_lifetime_check = var.ssl_cert_remaining_lifetime_check
+    location                          = var.location
+  })
+}
+
+data "azurerm_application_insights" "this" {
+  name                = var.application_insight_name
+  resource_group_name = var.resource_group
+}
+
+resource "azurerm_template_deployment" "this" {
+  name                = var.name
+  resource_group_name = var.resource_group
+
+  template_body = local.template_body
+
+  deployment_mode = "Incremental"
+}
+
+resource "azurerm_monitor_metric_alert" "this" {
+  name                = format("%s-%s", var.name, var.application_insight_name)
+  resource_group_name = var.resource_group
+  severity            = var.severity
+  scopes = [
+    data.azurerm_application_insights.this.id,
+    format("/subscriptions/%s/resourcegroups/%s/providers/microsoft.insights/webtests/%s-%s",
+      var.subscription_id,
+      var.resource_group,
+      var.name,
+    var.application_insight_name),
+  ]
+  description = "Web availability check alert triggered when it fails."
+
+  application_insights_web_test_location_availability_criteria {
+    web_test_id = format("/subscriptions/%s/resourcegroups/%s/providers/microsoft.insights/webtests/%s-%s",
+      var.subscription_id,
+      var.resource_group,
+      var.name,
+      var.application_insight_name
+    )
+    component_id          = data.azurerm_application_insights.this.id
+    failed_location_count = var.failed_location_count
+  }
+
+  dynamic "action" {
+    for_each = var.actions
+    content {
+      action_group_id = action.value["action_group_id"]
+    }
+  }
+
+  depends_on = [
+    azurerm_template_deployment.this,
+  ]
+}
