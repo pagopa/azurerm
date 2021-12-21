e2a220be78a957ef4851bd67762394c307ce06bc
uolter <walter.traspadini@pagopa.it>
2021-12-21 10:29:29 +0100

    fix local.name not declared.

--- application_insights_web_test_preview/main.tf
+++ application_insights_web_test_preview/main.tf
@@ -1,14 +1,14 @@
 locals {
-    subscription_id                   = var.subscription_id
-    web_test_name                     = var.name
-    resource_group                    = var.resource_group
-    application_insight_name          = var.application_insight_name
-    frequency                         = var.frequency
-    timeout                           = var.timeout
-    request_url                       = var.request_url
-    expected_http_status              = var.expected_http_status
-    ignore_http_status                = var.ignore_http_status
-    ssl_cert_remaining_lifetime_check = var.ssl_cert_remaining_lifetime_check
+  subscription_id                   = var.subscription_id
+  web_test_name                     = var.name
+  resource_group                    = var.resource_group
+  application_insight_name          = var.application_insight_name
+  frequency                         = var.frequency
+  timeout                           = var.timeout
+  request_url                       = var.request_url
+  expected_http_status              = var.expected_http_status
+  ignore_http_status                = var.ignore_http_status
+  ssl_cert_remaining_lifetime_check = var.ssl_cert_remaining_lifetime_check
 }
 
 
@@ -19,7 +19,7 @@ resource "azurerm_template_deployment" "this" {
   template_body = templatefile("${path.module}/template.tftpl",
     {
       subscription_id                   = local.subscription_id
-      web_test_name                     = local.name
+      web_test_name                     = local.web_test_name
       resource_group                    = local.resource_group
       application_insight_name          = local.application_insight_name
       frequency                         = local.frequency
@@ -27,7 +27,7 @@ resource "azurerm_template_deployment" "this" {
       request_url                       = local.request_url
       expected_http_status              = local.expected_http_status
       ignore_http_status                = local.ignore_http_status
-     ssl_cert_remaining_lifetime_check  = local.ssl_cert_remaining_lifetime_check
+      ssl_cert_remaining_lifetime_check = local.ssl_cert_remaining_lifetime_check
     }
   )
 
