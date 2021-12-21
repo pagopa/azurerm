79a1375218d8a462eb8fe56104389275563e0707
uolter <walter.traspadini@pagopa.it>
2021-12-21 10:26:07 +0100

    template param as local definition

--- application_insights_web_test_preview/main.tf
+++ application_insights_web_test_preview/main.tf
@@ -1,19 +1,33 @@
+locals {
+    subscription_id                   = var.subscription_id
+    web_test_name                     = var.name
+    resource_group                    = var.resource_group
+    application_insight_name          = var.application_insight_name
+    frequency                         = var.frequency
+    timeout                           = var.timeout
+    request_url                       = var.request_url
+    expected_http_status              = var.expected_http_status
+    ignore_http_status                = var.ignore_http_status
+    ssl_cert_remaining_lifetime_check = var.ssl_cert_remaining_lifetime_check
+}
+
+
 resource "azurerm_template_deployment" "this" {
   name                = var.name
   resource_group_name = var.resource_group
 
   template_body = templatefile("${path.module}/template.tftpl",
     {
-      subscription_id                   = var.subscription_id,
-      web_test_name                     = var.name,
-      resource_group                    = var.resource_group
-      application_insight_name          = var.application_insight_name,
-      frequency                         = var.frequency,
-      timeout                           = var.timeout,
-      request_url                       = var.request_url,
-      expected_http_status              = var.expected_http_status,
-      ignore_http_status                = var.ignore_http_status,
-      ssl_cert_remaining_lifetime_check = var.ssl_cert_remaining_lifetime_check,
+      subscription_id                   = local.subscription_id
+      web_test_name                     = local.name
+      resource_group                    = local.resource_group
+      application_insight_name          = local.application_insight_name
+      frequency                         = local.frequency
+      timeout                           = local.timeout
+      request_url                       = local.request_url
+      expected_http_status              = local.expected_http_status
+      ignore_http_status                = local.ignore_http_status
+     ssl_cert_remaining_lifetime_check  = local.ssl_cert_remaining_lifetime_check
     }
   )
 
