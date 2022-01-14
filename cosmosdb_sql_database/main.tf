ef84e85080b2cfe3499a5ac6e454b74daec36dab
uolter <walter.traspadini@pagopa.it>
2022-01-14 12:00:13 +0100

    first draft module cosmos sql database

---
+++ cosmosdb_sql_database/main.tf
@@ -0,0 +1,6 @@
+resource "azurerm_cosmosdb_sql_database" "this" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  account_name        = var.account_name
+  throughput          = var.throughput
+}
---
+++ cosmosdb_sql_database/outputs.tf
@@ -0,0 +1,9 @@
+
+
+output "id" {
+  value = azurerm_cosmosdb_sql_database.this.id
+}
+
+output "name" {
+  value = azurerm_cosmosdb_sql_database.this.name
+}
