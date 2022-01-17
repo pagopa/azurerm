d91e7e108b8b0846e9bcf2ce50f90a44b9083042
pasqualedevita <>
2022-01-17 17:45:19 +0100

    fix-azurerm_app_service_slot_virtual_network_swift_connection

--- function_app_slot/main.tf
+++ function_app_slot/main.tf
@@ -78,7 +78,8 @@ data "azurerm_function_app_host_keys" "this" {
   depends_on          = [azurerm_function_app_slot.this]
 }
 
-resource "azurerm_app_service_virtual_network_swift_connection" "this" {
+resource "azurerm_app_service_slot_virtual_network_swift_connection" "this" {
+  slot_name      = azurerm_function_app_slot.this.name
   app_service_id = azurerm_function_app_slot.this.id
   subnet_id      = var.subnet_id
 }
