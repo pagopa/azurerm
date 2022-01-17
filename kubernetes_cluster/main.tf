5a8856065c68b94f36a19b99d3dbc0f317e555d6
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-01-17 10:11:30 +0100

    aks: added comments to improve docs

--- kubernetes_cluster/main.tf
+++ kubernetes_cluster/main.tf
@@ -29,6 +29,7 @@ resource "azurerm_kubernetes_cluster" "this" {
   automatic_channel_upgrade       = var.automatic_channel_upgrade
   api_server_authorized_ip_ranges = var.api_server_authorized_ip_ranges #tfsec:ignore:AZU008
 
+  # managed identity type: https://docs.microsoft.com/en-us/azure/aks/use-managed-identity
   identity {
     type = "SystemAssigned"
   }
@@ -51,7 +52,6 @@ resource "azurerm_kubernetes_cluster" "this" {
     }
   }
 
-
   role_based_access_control {
     enabled = var.rbac_enabled
     azure_active_directory {
@@ -59,7 +59,6 @@ resource "azurerm_kubernetes_cluster" "this" {
       admin_group_object_ids = var.aad_admin_group_ids
     }
   }
-
 
   addon_profile {
 
@@ -94,6 +93,9 @@ resource "azurerm_kubernetes_cluster" "this" {
   tags = var.tags
 }
 
+#
+# Role Assigments
+#
 resource "azurerm_role_assignment" "aks" {
   scope                = azurerm_kubernetes_cluster.this.id
   role_definition_name = "Monitoring Metrics Publisher"
@@ -112,80 +114,4 @@ resource "azurerm_role_assignment" "vnet_outbound_role" {
   scope                = each.key
   role_definition_name = "Network Contributor"
   principal_id         = azurerm_kubernetes_cluster.this.identity[0].principal_id
-}
-
-resource "azurerm_monitor_metric_alert" "this" {
-  for_each = var.metric_alerts
-
-  name                = format("%s-%s", azurerm_kubernetes_cluster.this.name, upper(each.key))
-  resource_group_name = var.resource_group_name
-  scopes              = [azurerm_kubernetes_cluster.this.id]
-  frequency           = each.value.frequency
-  window_size         = each.value.window_size
-  enabled             = var.alerts_enabled
-
-  dynamic "action" {
-    for_each = var.action
-    content {
-      # action_group_id - (required) is a type of string
-      action_group_id = action.value["action_group_id"]
-      # webhook_properties - (optional) is a type of map of string
-      webhook_properties = action.value["webhook_properties"]
-    }
-  }
-
-  criteria {
-    aggregation      = each.value.aggregation
-    metric_namespace = each.value.metric_namespace
-    metric_name      = each.value.metric_name
-    operator         = each.value.operator
-    threshold        = each.value.threshold
-
-    dynamic "dimension" {
-      for_each = each.value.dimension
-      content {
-        name     = dimension.value.name
-        operator = dimension.value.operator
-        values   = dimension.value.values
-      }
-    }
-  }
-}
-
-resource "azurerm_monitor_diagnostic_setting" "aks" {
-  count                      = var.sec_log_analytics_workspace_id != null ? 1 : 0
-  name                       = "LogSecurity"
-  target_resource_id         = azurerm_kubernetes_cluster.this.id
-  log_analytics_workspace_id = var.sec_log_analytics_workspace_id
-  storage_account_id         = var.sec_storage_id
-
-  log {
-    category = "kube-audit"
-    enabled  = true
-
-    retention_policy {
-      enabled = true
-      days    = 365
-    }
-  }
-
-  log {
-    category = "kube-audit-admin"
-    enabled  = true
-
-    retention_policy {
-      enabled = true
-      days    = 365
-    }
-  }
-
-  metric {
-    category = "AllMetrics"
-    enabled  = false
-
-    retention_policy {
-      days    = 0
-      enabled = false
-    }
-  }
 }
