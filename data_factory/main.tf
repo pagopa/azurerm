1e1c657de72288030f87341be2e3254e558a8552
Walter Traspadini <walter.traspadini@pagopa.it>
2022-01-19 21:00:03 +0100

    Update data_factory/main.tf

--- data_factory/main.tf
+++ data_factory/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_data_factory" "this" {
   name                   = format("%s-data-factory", var.name)
   location               = var.location
   resource_group_name    = var.resource_group_name
-  public_network_enabled = !(var.private_endpoint.enabled)
+  public_network_enabled = !var.private_endpoint.enabled
 
   github_configuration {
     # (Required) Specifies the GitHub account name
