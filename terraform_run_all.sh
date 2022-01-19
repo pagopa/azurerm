d3a29435d14584a6c43e8112079812af4a307ad7
pasqualedevita <>
2022-01-19 11:59:32 +0100

    fix provider required

--- terraform_run_all.sh
+++ terraform_run_all.sh
@@ -4,6 +4,7 @@ TAG=latest
 for f in *; do
   if [ -d "$f" ]; then
     echo "$f"
+    cp ".utils/provider.tf" "$f/"
     cd "$f"
     docker run -v $(pwd):/tmp -w /tmp hashicorp/terraform:$TAG $1
     cd ..
