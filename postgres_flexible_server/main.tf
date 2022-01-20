0428fb37708649039651a5dc8caf3c0ce4d23feb
Giovanni <g.mancini.0@gmail.com>
2022-01-20 16:12:34 +0100

    [CEN-1302] Remove commented code

--- postgres_flexible_server/main.tf
+++ postgres_flexible_server/main.tf
@@ -10,50 +10,6 @@
 #   monitor_metric_alert_criteria         = var.alerts_enabled ? var.monitor_metric_alert_criteria : {}
 # }
 
-# provider "azurerm" {
-#   features {}
-# }
-
-# resource "azurerm_resource_group" "example" {
-#   name     = "example-resources"
-#   location = "West Europe"
-# }
-
-# resource "azurerm_virtual_network" "example" {
-#   name                = "example-vn"
-#   location            = azurerm_resource_group.example.location
-#   resource_group_name = azurerm_resource_group.example.name
-#   address_space       = ["10.0.0.0/16"]
-# }
-
-# resource "azurerm_subnet" "example" {
-#   name                 = "example-sn"
-#   resource_group_name  = azurerm_resource_group.example.name
-#   virtual_network_name = azurerm_virtual_network.example.name
-#   address_prefixes     = ["10.0.2.0/24"]
-#   service_endpoints    = ["Microsoft.Storage"]
-#   delegation {
-#     name = "fs"
-#     service_delegation {
-#       name = "Microsoft.DBforPostgreSQL/flexibleServers"
-#       actions = [
-#         "Microsoft.Network/virtualNetworks/subnets/join/action",
-#       ]
-#     }
-#   }
-# }
-# resource "azurerm_private_dns_zone" "example" {
-#   name                = "example.postgres.database.azure.com"
-#   resource_group_name = azurerm_resource_group.example.name
-# }
-
-# resource "azurerm_private_dns_zone_virtual_network_link" "example" {
-#   name                  = "exampleVnetZone.com"
-#   private_dns_zone_name = azurerm_private_dns_zone.example.name
-#   virtual_network_id    = azurerm_virtual_network.example.id
-#   resource_group_name   = azurerm_resource_group.example.name
-# }
-
 resource "azurerm_postgresql_flexible_server" "this" {
 
   name                = var.name
