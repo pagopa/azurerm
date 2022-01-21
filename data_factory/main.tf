b71e64e29f96df917b1ebe870c2df725d39db5f3
Giovanni <g.mancini.0@gmail.com>
2022-01-21 15:54:53 +0100

    [CEN-1293] Fix data factory name

--- data_factory/main.tf
+++ data_factory/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_data_factory" "this" {
-  name                   = format("%s-data-factory", var.name)
+  name                   = var.name
   location               = var.location
   resource_group_name    = var.resource_group_name
   public_network_enabled = !var.private_endpoint.enabled
