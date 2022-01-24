8e1876fbcb146799de180251fb3ad0fb2f9e144a
uolter <walter.traspadini@pagopa.it>
2022-01-24 11:54:07 +0100

    resolved conflict

--- terraform_run_all.sh
+++ terraform_run_all.sh
@@ -7,7 +7,11 @@ for f in *; do
 <<<<<<< HEAD
     cp ".utils/provider.tf" "$f/"
 =======
+<<<<<<< HEAD
 >>>>>>> 6572bc7 (Add init for tf validate (#165))
+=======
+>>>>>>> 542b7f4 (Add init for tf validate (#165))
+>>>>>>> e8a296e (resolved conflict)
     cd "$f"
     docker run -v $(pwd):/tmp -w /tmp hashicorp/terraform:$TAG $1
     cd ..
