96422d43b4ecdb2e7c711cafae4812e6eb51210d
Giovanni <g.mancini.0@gmail.com>
2022-01-24 15:45:32 +0100

    Cen 1293 data factory module (#171)
    * [CEN-1293] New Module to create Az data Factories with private endpoint
    
    * [CEN-1293] Fix names of managed private endpoints
    
    * [CEN-1293] Fix name of variable name
    
    * [CEN-1293] Fix name of github_conf.root_folder
    
    * [CEN-1293] Fix for_each syntax
    
    * [CEN-1293] Fix for_each syntax (use value vs key)
    
    * [CEN-1293] define the resources to be accessed via private endpoint as a map
    
    * [CEN-1293] simplify variable type
    
    * [CEN-1293] fix typo (and type) in github_conf variable definition
    
    * [CEN-1293] support different resoure groups for resources to be created and existing private dns zone
    
    * [CEN-1293] support different names for different resources in managed private endpoint
    
    * [CEN-1293] Use this instead of useless resource names
    
    * [CEN-1293] Remove name_prefix
    
    * [CEN-1293] Make optional private endpoint
    
    * [CEN-1293] Run terraform fmt
    
    * [CEN-1293] Fix resource indexing
    
    * [CEN-1293] Fix count condition
    
    * Update data_factory/main.tf
    
    * [CEN-1293] Add tags
    
    * [CEN-1293] rename dns_a_record_name variable
    
    * [CEN-1293] rename azurerm private DNS A record resource
    
    * [CEN-1293] fix typo
    
    * [CEN-1293] Fix data factory name
    
    * Update data_factory/main.tf
    
    * Update data_factory/main.tf
    
    Co-authored-by: Walter Traspadini <walter.traspadini@pagopa.it>
    Co-authored-by: Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>

---
+++ data_factory/main.tf
@@ -0,0 +1,74 @@
+resource "azurerm_data_factory" "this" {
+  name                   = var.name
+  location               = var.location
+  resource_group_name    = var.resource_group_name
+  public_network_enabled = !var.private_endpoint.enabled
+
+  github_configuration {
+    # (Required) Specifies the GitHub account name
+    account_name = var.github_conf.account_name
+    # (Required) Specifies the collaboration branch of the repository to get code from.
+    # The poublish branch is automatically set to adf_publish
+    branch_name = var.github_conf.branch_name
+    # (Required) Specifies the GitHub Enterprise host name. 
+    # For example: https://github.mydomain.com. Use https://github.com for open source repositories
+    git_url = var.github_conf.git_url
+    # (Required) Specifies the name of the git repository
+    repository_name = var.github_conf.repository_name
+    # (Required) Specifies the root folder within the repository. Set to / for the top level.
+    root_folder = var.github_conf.root_folder
+  }
+
+  # Still doesn't work: https://github.com/hashicorp/terraform-provider-azurerm/issues/12949
+  managed_virtual_network_enabled = true
+
+  tags = var.tags
+
+}
+
+resource "azurerm_private_endpoint" "this" {
+
+  count = var.private_endpoint.enabled ? 1 : 0
+
+  name                = format("%s-private-endpoint", var.name)
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.private_endpoint.subnet_id
+
+  private_dns_zone_group {
+    name = format("%s-private-dns-zone-group", var.name)
+    # One of the concatenated arrays is empty
+    private_dns_zone_ids = [var.private_endpoint.private_dns_zone.id]
+  }
+
+  private_service_connection {
+    name                           = format("%s-private-service-connection", var.name)
+    private_connection_resource_id = azurerm_data_factory.this.id
+    is_manual_connection           = false
+    subresource_names              = ["datafactory"]
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_private_dns_a_record" "this" {
+
+  count = var.custom_domain_enabled == null ? 0 : 1
+
+  name                = var.custom_domain_enabled
+  zone_name           = var.private_endpoint.private_dns_zone.name
+  resource_group_name = var.private_endpoint.private_dns_zone.rg
+  ttl                 = 300
+  records             = azurerm_private_endpoint.this[count.index].private_service_connection.*.private_ip_address
+
+  tags = var.tags
+}
+
+resource "azurerm_data_factory_managed_private_endpoint" "this" {
+  for_each           = var.resources_managed_private_enpoint
+  name               = replace(format("%s-%s-mng-private-endpoint", var.name, substr(sha256(each.key), 0, 3)), "-", "_")
+  data_factory_id    = azurerm_data_factory.this.id
+  target_resource_id = each.key
+  subresource_name   = each.value
+}
+
