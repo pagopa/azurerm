1a8c35c2d77c4d36cbbfd58d7a36f385674e5a85
uolter <walter.traspadini@pagopa.it>
2022-01-24 11:55:51 +0100

    resolved conflict

--- terraform_run_all.sh
+++ terraform_run_all.sh
@@ -4,10 +4,7 @@ TAG=latest
 for f in *; do
   if [ -d "$f" ]; then
     echo "$f"
-<<<<<<< HEAD
     cp ".utils/provider.tf" "$f/"
-=======
->>>>>>> 542b7f4 (Add init for tf validate (#165))
     cd "$f"
     docker run -v $(pwd):/tmp -w /tmp hashicorp/terraform:$TAG $1
     cd ..
