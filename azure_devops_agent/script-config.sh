dbfc9129bb15f45134a3ceb41a1b1544ba21d9ff
pasqualedevita <>
2022-02-03 10:04:30 +0100

    add docker to devops agent

--- azure_devops_agent/script-config.sh
+++ azure_devops_agent/script-config.sh
@@ -22,4 +22,17 @@ apt-get -y update
 apt-get -y install azure-cli
 
 # install python package index (pip)
+
 apt-get -y install python-pip
+
+# install docker from https://docs.docker.com/engine/install/ubuntu/
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg |
+    gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get -y update
+apt-get -y install docker-ce docker-ce-cli containerd.io
