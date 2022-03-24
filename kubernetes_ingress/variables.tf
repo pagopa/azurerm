443091dfeda78a691be8c2f2c4f865f7fcc0ed8b
Matteo Cellucci <matteo.cellucci@keypartner.com>
2022-03-24 09:20:32 +0100

    Update variables description of kubernetes modules

--- kubernetes_ingress/variables.tf
+++ kubernetes_ingress/variables.tf
@@ -9,7 +9,7 @@ variable "rules" {
     service_name = string
     service_port = number
   }))
-  description = "Ingress rules."
+  description = "List of ingress rules."
 }
 
 variable "name" {
@@ -19,32 +19,32 @@ variable "name" {
 
 variable "host" {
   type        = string
-  description = "Ingress host."
+  description = "Ingress host, it supports only one host per ingress."
 }
 
 variable "tenant_id" {
   type        = string
-  description = "Azure tenant id of the KeyVault with the certificate."
+  description = "Azure tenant id of the KeyVault with the TLS certificate."
 }
 
 variable "location" {
   type        = string
-  description = "Location of the Kubernetes cluster"
+  description = "Location of the Kubernetes cluster."
 }
 
 variable "resource_group_name" {
   type        = string
-  description = "Resource group of the Kubernetes cluster"
+  description = "Resource group of the Kubernetes cluster."
 }
 
 variable "cluster_name" {
   type        = string
-  description = "Name of the Kubernetes cluster"
+  description = "Name of the Kubernetes cluster."
 }
 
 variable "key_vault" {
   type        = any
-  description = "KeyVault azurerm resource"
+  description = "KeyVault azurerm resource."
 }
 
 locals {
--- kubernetes_pod_identity/variables.tf
+++ kubernetes_pod_identity/variables.tf
@@ -1,26 +1,26 @@
 variable "resource_group_name" {
   type        = string
-  description = "Resource group of the Kubernetes cluster"
+  description = "Resource group of the Kubernetes cluster."
 }
 
 variable "location" {
   type        = string
-  description = "Location of the Kubernetes cluster"
+  description = "Location of the Kubernetes cluster."
 }
 
 variable "key_vault" {
   type        = any
-  description = "KeyVault azurerm resource"
+  description = "KeyVault azurerm resource where the identity will connect to."
 }
 
 variable "tenant_id" {
   type        = string
-  description = "Azure tenant id of the KeyVault with the certificate."
+  description = "Azure tenant id of the given KeyVault."
 }
 
 variable "cluster_name" {
   type        = string
-  description = "Name of the Kubernetes cluster"
+  description = "Name of the Kubernetes cluster."
 }
 
 variable "namespace" {
@@ -30,24 +30,23 @@ variable "namespace" {
 
 variable "identity_name" {
   type        = string
-  description = "Kubernetes namespace."
+  description = "Name to use for the identity."
 }
 
 variable "secret_permissions" {
   type        = list(string)
-  description = ""
+  description = "API permissions of the identity to access secrets"
   default     = []
 }
 
 variable "key_permissions" {
   type        = list(string)
-  description = ""
+  description = "API permissions of the identity to access keys"
   default     = []
 }
 
 variable "certificate_permissions" {
   type        = list(string)
-  description = ""
+  description = "API permissions of the identity to access certificates"
   default     = []
 }
-
