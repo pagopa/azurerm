1cada41cd98c6031ec081ef6fa79bc668ddd4fd9
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-03-29 17:40:40 +0200

    added HA configurations and simplified network

--- postgres_flexible_server/main.tf
+++ postgres_flexible_server/main.tf
@@ -11,23 +11,37 @@ resource "azurerm_postgresql_flexible_server" "this" {
   backup_retention_days        = var.backup_retention_days
   geo_redundant_backup_enabled = var.geo_redundant_backup_enabled
   create_mode                  = var.create_mode
-  zone                   = var.zone
+  zone                         = var.zone
 
   #
   # Network
   #
 
   # The provided subnet should not have any other resource deployed in it and this subnet will be delegated to the PostgreSQL Flexible Server, if not already delegated.
-  delegated_subnet_id = var.private_endpoint.subnet_id
+  delegated_subnet_id = var.private_endpoint_enabled ? var.delegated_subnet_id : null
   #  private_dns_zobe_id will be required when setting a delegated_subnet_id
-  private_dns_zone_id = var.private_endpoint.private_dns_zone.id
+  private_dns_zone_id = var.private_endpoint_enabled ? var.private_dns_zone_id : null
 
   administrator_login    = var.administrator_login
   administrator_password = var.administrator_password
 
   storage_mb = var.storage_mb
-
   sku_name = var.sku_name
+
+  dynamic "high_availability" {
+    for_each = var.high_availability_enabled && var.standby_availability_zone != null ? ["dummy"] : []
+    
+    mode = "ZoneRedundant" #only possible value
+    standby_availability_zone = var.standby_availability_zone
+  }
+
+  dynamic "maintenance_window" {
+    for_each = var.maintenance_window_config != null ? ["dummy"] : []
+    
+    day_of_week = var.maintenance_window_config.day_of_week
+    start_hour = var.maintenance_window_config.start_hour
+    start_minute = var.maintenance_window_config.start_minute
+  }
 
   tags = var.tags
 
--- postgres_flexible_server/variables.tf
+++ postgres_flexible_server/variables.tf
@@ -21,19 +21,58 @@ variable "db_version" {
 #
 # Network
 #
-variable "private_endpoint" {
-  type = object({
-    enabled   = bool
-    subnet_id = string # The ID of the virtual network subnet to create the PostgreSQL Flexible Server. The provided subnet should not have any other resource deployed in it and this subnet will be delegated to the PostgreSQL Flexible Server, if not already delegated.
-    private_dns_zone = object({
-      id   = string # The ID of the private dns zone to create the PostgreSQL Flexible Server.
-      name = string
-      rg   = string
-    })
-  })
-  description = "Enable private endpoint with required params"
+
+variable "private_endpoint_enabled" {
+  type = bool
+  description = "Is this instance private only?"
 }
 
+variable "private_dns_zone_id" {
+  type = string
+  default = null
+  description = "(Optional) The ID of the private dns zone to create the PostgreSQL Flexible Server. Changing this forces a new PostgreSQL Flexible Server to be created."
+}
+
+variable "delegated_subnet_id" {
+  type = string
+  default = null
+  description = "(Optional) The ID of the virtual network subnet to create the PostgreSQL Flexible Server. The provided subnet should not have any other resource deployed in it and this subnet will be delegated to the PostgreSQL Flexible Server, if not already delegated."
+}
+
+#
+# ♊️ High Availability
+#
+variable "high_availability_enabled" {
+  type = bool
+  description = "(Required) Is the High Availability Enabled"
+}
+
+variable "standby_availability_zone" {
+  type = number
+  default = null
+  description = "(Optional) Specifies the Availability Zone in which the standby Flexible Server should be located."
+}
+
+variable "maintenance_window_config" {
+  type = object({
+    day_of_week = number
+    start_hour = number
+    start_minute = number
+  })
+
+  default = {
+    day_of_week = 6
+    start_hour = 2
+    start_minute = 0
+  }
+
+  description = "(Optional) Allows the configuration of the maintenance window, if not configured default is sunday@2.00am"
+  
+}
+
+#
+# Administration
+#
 variable "administrator_login" {
   type        = string
   description = "The Administrator Login for the PostgreSQL Flexible Server. Required when create_mode is Default."
@@ -86,4 +125,8 @@ variable "zone" {
 
 variable "tags" {
   type = map(any)
+}
+
+locals {
+  high_availability_zone = var.zone != null && var.zone == 2 ? 3 : 1
 }
