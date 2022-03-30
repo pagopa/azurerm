d59b29dc9f3bad0e5826f3b8ae0b092b74d7c1e5
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-03-30 14:38:21 +0200

    added provider null resources

--- postgres_flexible_server/versions.tf
+++ postgres_flexible_server/versions.tf
@@ -6,5 +6,9 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 2.80.0, <= 2.99.0"
     }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.1.1"
+    }
   }
 }
