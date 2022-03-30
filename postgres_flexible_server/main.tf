3d71f1ebcf0f371a9353e332b2c0f27797454c57
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-03-30 10:31:37 +0200

    added pgbouncer

--- postgres_flexible_server/main.tf
+++ postgres_flexible_server/main.tf
@@ -62,5 +62,5 @@ resource "azurerm_postgresql_flexible_server_configuration" "pgbouncer_enabled" 
   
   name      = "pgbouncer.enabled"
   server_id = azurerm_postgresql_flexible_server.this.id
-  value     = "on"
+  value     = "True"
 }
