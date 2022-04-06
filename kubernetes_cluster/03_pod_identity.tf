a88445bc5f68dc196acbb7b65abc5f5378c5393c
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-04-06 11:01:30 +0200

    add pod-identity with checks for aks-preview

--- kubernetes_cluster/03_pod_identity.tf
+++ kubernetes_cluster/03_pod_identity.tf
@@ -1,31 +1,43 @@
-# resource "null_resource" "enable_pod_identity" {
-#   # needs az extension
-#   # see https://docs.microsoft.com/en-us/azure/aks/use-azure-ad-pod-identity#before-you-begin
+resource "null_resource" "enable_pod_identity" {
+  # needs az extension
+  # see https://docs.microsoft.com/en-us/azure/aks/use-azure-ad-pod-identity#before-you-begin
 
-#   count = var.enable_azure_pod_identity ? 1 : 0
+  count = var.enable_azure_pod_identity ? 1 : 0
 
-#   triggers = {
-#     resource_group_name = var.resource_group_name
-#     cluster_name        = azurerm_kubernetes_cluster.this.name
-#   }
+  triggers = {
+    resource_group_name = var.resource_group_name
+    cluster_name        = azurerm_kubernetes_cluster.this.name
+  }
 
-#   provisioner "local-exec" {
-#     command = <<EOT
-#       az aks update \
-#         -g ${self.triggers.resource_group_name} \
-#         -n ${self.triggers.cluster_name} \
-#         --enable-pod-identity
-#     EOT
-#   }
+  provisioner "local-exec" {
+    command = <<EOT
+      if az extension list-available | grep aks-preview > /dev/null
+      then
+        az extension add --name aks-preview
 
-#   provisioner "local-exec" {
-#     when    = destroy
-#     command = <<EOT
-#       az aks update \
-#         -g ${self.triggers.resource_group_name} \
-#         -n ${self.triggers.cluster_name} \
-#         --disable-pod-identity
-#     EOT
-#   }
-# }
+        az aks update \
+        -g ${self.triggers.resource_group_name} \
+        -n ${self.triggers.cluster_name} \
+        --enable-pod-identity
+      else
+        echo "addon: aks-preview not avaible"
+      fi
+    EOT
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = <<EOT
+      if az extension list-available | grep aks-preview > /dev/null
+      then
+        az aks update \
+          -g ${self.triggers.resource_group_name} \
+          -n ${self.triggers.cluster_name} \
+          --disable-pod-identity
+      else
+        echo "addon: aks-preview not avaible"
+      fi
+    EOT
+  }
+}
 
