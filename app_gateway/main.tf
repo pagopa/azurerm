ffcec1ce3f4cdf1caafbafce257beebb308daf9b
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2022-05-05 16:53:21 +0200

    fix(app_gateway): Use only GCM ciphersuites (#228)

--- app_gateway/main.tf
+++ app_gateway/main.tf
@@ -254,8 +254,6 @@ resource "azurerm_application_gateway" "this" {
     cipher_suites = [
       "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
       "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
-      "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA",
-      "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA"
     ]
     min_protocol_version = "TLSv1_2"
   }
