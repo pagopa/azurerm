022ee96c1b214666370a298f6848e1a1b6962537
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2022-07-14 18:12:48 +0200

    load test (#247)
    Co-authored-by: pasqualedevita <>

--- azure_devops_agent/script-config.sh
+++ azure_devops_agent/script-config.sh
@@ -58,3 +58,8 @@ apt-get -y install helm
 # install zip unzip
 apt-get -y update
 apt-get -y install zip unzip
+
+sysctl -w net.ipv4.ip_local_port_range="1024 65535"
+sysctl -w net.ipv4.tcp_tw_reuse=1
+sysctl -w net.ipv4.tcp_timestamps=1
+ulimit -n 250000
