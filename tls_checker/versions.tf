c13b547cb90cbb0fc60b5299fcb088a1ec0b177d
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2022-10-28 15:40:05 +0200

    Update versions.tf

--- tls_checker/versions.tf
+++ tls_checker/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.80.0, <= 2.99.0"
+      version = ">= 2.80.0, <= 3.28.0"
     }
     helm = {
       source  = "hashicorp/helm"
