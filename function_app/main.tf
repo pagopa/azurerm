0e516a34aeffa1c252cd4f0f451f4a11cc64d4ca
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2022-12-09 13:44:49 +0100

    fix(function_app): Fix create blob container management policy tolist (#272)
    * fix tolist
    
    * retry
    
    * fx
    
    Co-authored-by: pasqualedevita <>

--- function_app/main.tf
+++ function_app/main.tf
@@ -60,7 +60,7 @@ resource "azurerm_storage_container" "internal_container" {
 
 module "storage_account_durable_function_management_policy" {
   count  = length(local.internal_containers) == 0 ? 0 : 1
-  source = "git::https://github.com/pagopa/azurerm.git//storage_management_policy?ref=v2.7.0"
+  source = "git::https://github.com/pagopa/azurerm.git//storage_management_policy?ref=v3.6.1"
 
   storage_account_id = module.storage_account_durable_function[0].id
 
