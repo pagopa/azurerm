c3f98b88afaf2cd1ce357d5f563824a8584e0f32
gquadrati <greta.quadrati@pagopa.it>
2022-12-14 15:38:04 +0100

    fix name format

--- function_app/main.tf
+++ function_app/main.tf
@@ -295,7 +295,7 @@ resource "azurerm_app_service_virtual_network_swift_connection" "this" {
 resource "azurerm_monitor_metric_alert" "function_app_health_check" {
   count = var.enable_healthcheck ? 1 : 0
 
-  name                = "[${var.domain != null ? "${var.domain} |" : ""} ${azurerm_function_app.this.name}] Health Check Failed"
+  name                = "[${var.domain != null ? "${var.domain} | " : ""}${azurerm_function_app.this.name}] Health Check Failed"
   resource_group_name = var.resource_group_name
   scopes              = [azurerm_function_app.this.id]
   description         = "Function availability is under threshold level. Runbook: -"
