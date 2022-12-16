a2c483e0df6f061917e13c9ee7065163e72e30c7
gquadrati <greta.quadrati@pagopa.it>
2022-12-16 16:41:25 +0100

    add Provisioned Throughput Exceeded alert to cosmos db

--- cosmosdb_account/main.tf
+++ cosmosdb_account/main.tf
@@ -1,6 +1,7 @@
 resource "azurerm_cosmosdb_account" "this" {
   name                      = var.name
   location                  = var.location
+  domain                    = var.domain
   resource_group_name       = var.resource_group_name
   offer_type                = var.offer_type
   kind                      = var.kind
@@ -167,3 +168,62 @@ resource "azurerm_management_lock" "this" {
   lock_level = "CanNotDelete"
   notes      = "This items can't be deleted in this subscription!"
 }
+
+
+# -----------------------------------------------
+# Alerts
+# -----------------------------------------------
+
+resource "azurerm_monitor_metric_alert" "cosmos_db_provisioned_throughput_exceeded" {
+  count = var.enable_provisioned_throughput_exceeded_alert ? 1 : 0
+
+  name                = "[${var.domain != null ? "${var.domain} | " : ""}${azurerm_cosmosdb_account.this.name}] Low Availability"
+  resource_group_name = var.resource_group_name
+  scopes              = [azurerm_cosmosdb_account.this.id]
+  description         = "A collection throughput (RU/s) exceed provisioned throughput. Please, consider to increase RU. Runbook: not needed."
+  severity            = 0
+  window_size         = "PT5M"
+  frequency           = "PT5M"
+  auto_mitigate       = false
+
+
+  # Metric info
+  # https://learn.microsoft.com/en-us/azure/azure-monitor/essentials/metrics-supported#microsoftdocumentdbdatabaseaccounts
+  criteria {
+    metric_namespace       = "Microsoft.DocumentDB/databaseAccounts"
+    metric_name            = "TotalRequestUnits"
+    aggregation            = "Total"
+    operator               = "GreaterThan"
+    threshold              = var.provisioned_throughput_exceeded_threshold
+    skip_metric_validation = false
+
+
+    dimension {
+      name     = "Region"
+      operator = "Include"
+      values   = ["West Europe"]
+    }
+    dimension {
+      name     = "StatusCode"
+      operator = "Include"
+      values   = ["429"]
+    }
+    dimension {
+      name     = "CollectionName"
+      operator = "Include"
+      values   = ["*"]
+    }
+
+  }
+
+  dynamic "action" {
+    for_each = var.action
+    content {
+      action_group_id    = action.value["action_group_id"]
+      webhook_properties = action.value["webhook_properties"]
+    }
+  }
+
+  tags = var.tags
+}
+
