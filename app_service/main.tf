07befcdb71c33eb599ef1e49b59cf10a776cbe83
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2022-12-23 10:41:36 +0100

    fix: Override vnet restrictions name in app service and function app (#278)
    fix name app service, function app vnet restrictions
    
    Co-authored-by: pasqualedevita <>

--- app_service/main.tf
+++ app_service/main.tf
@@ -52,6 +52,7 @@ resource "azurerm_app_service" "this" {
       content {
         ip_address                = null
         virtual_network_subnet_id = subnet.value
+        name                      = ""
       }
     }
 
@@ -62,6 +63,7 @@ resource "azurerm_app_service" "this" {
       content {
         ip_address                = ip.value
         virtual_network_subnet_id = null
+        name                      = ""
       }
     }
 
