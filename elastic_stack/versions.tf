cfb7c0784a8ed66db3ff5fc325eecadefc0f024c
ma_cloud_arch79 <matteoalongi79@gmail.com>
2022-12-27 12:08:07 +0100

    provider fix

--- elastic_stack/versions.tf
+++ elastic_stack/versions.tf
@@ -1,10 +1,12 @@
 terraform {
-  required_version = ">= 1.1.0"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.80.0, <= 2.99.0"
+      version = "= 2.99.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "= 2.11.0"
     }
   }
 }
