5bd464eef35ee1964fd75868badb9aa8f596d9e7
ma_cloud_arch79 <94049389+matteoalongi@users.noreply.github.com>
2022-12-27 15:54:42 +0100

    feat: Add elastic stack module (#280)
    * add elastic module
    
    * provider fix
    
    * fixing
    
    * variables fix
    
    * yaml path

---
+++ elastic_stack/main.tf
@@ -0,0 +1,54 @@
+resource "kubernetes_manifest" "elastic_manifest" {
+  field_manager {
+    force_conflicts = true
+  }
+  computed_fields = ["metadata.labels", "metadata.annotations", "spec", "status"]
+  manifest = yamldecode(templatefile("${path.module}/yaml/elastic.yaml", {
+    num_node_balancer      = var.balancer_node_number
+    num_node_master        = var.master_node_number
+    num_node_hot           = var.hot_node_number
+    num_node_warm          = var.warm_node_number
+    num_node_cold          = var.cold_node_number
+    storage_size_balancer  = var.balancer_storage_size
+    storage_size_master    = var.master_storage_size
+    storage_size_hot       = var.hot_storage_size
+    storage_size_warm      = var.warm_storage_size
+    storage_size_cold      = var.cold_storage_size
+    storage_class_balancer = var.balancer_storage_class
+    storage_class_master   = var.master_storage_size
+    storage_class_hot      = var.hot_storage_size
+    storage_class_warm     = var.warm_storage_size
+    storage_class_cold     = var.cold_storage_size
+  }))
+}
+
+resource "kubernetes_manifest" "kibana_manifest" {
+  field_manager {
+    force_conflicts = true
+  }
+  computed_fields = ["metadata.labels", "metadata.annotations", "spec", "status"]
+  manifest = yamldecode(templatefile("${path.module}/yaml/kibana.yaml", {
+    external_domain = var.kibana_external_domain
+
+  }))
+}
+
+resource "kubernetes_manifest" "ingress_manifest" {
+  manifest = yamldecode(file("${path.module}/yaml/ingress.yaml"))
+}
+
+resource "kubernetes_manifest" "apm_manifest" {
+  field_manager {
+    force_conflicts = true
+  }
+  computed_fields = ["metadata.labels", "metadata.annotations", "spec", "status"]
+  manifest        = yamldecode(file("${path.module}/yaml/apm.yaml"))
+}
+
+resource "kubernetes_manifest" "secret_manifest" {
+  field_manager {
+    force_conflicts = true
+  }
+  computed_fields = ["metadata.labels", "metadata.annotations", "spec", "status"]
+  manifest        = yamldecode(templatefile("${path.module}/yaml/SecretProvider.yaml", {}))
+}
---
+++ elastic_stack/variables.tf
@@ -0,0 +1,89 @@
+variable "balancer_node_number" {
+  description = "Number of balancer nodes"
+  type        = string
+  default     = "0"
+}
+
+variable "master_node_number" {
+  description = "Number of master nodes"
+  type        = string
+  default     = "1"
+}
+
+variable "hot_node_number" {
+  description = "Number of hot nodes"
+  type        = string
+  default     = "1"
+}
+
+variable "warm_node_number" {
+  description = "Number of warm nodes"
+  type        = string
+  default     = "0"
+}
+
+variable "cold_node_number" {
+  description = "Number of cold nodes"
+  type        = string
+  default     = "0"
+}
+
+variable "balancer_storage_size" {
+  description = "Storage size of balancer node in GB"
+  type        = string
+  default     = "0"
+}
+
+variable "master_storage_size" {
+  description = "Storage size of master node in GB"
+  type        = string
+  default     = "20"
+}
+
+variable "hot_storage_size" {
+  description = "Storage size of hot node in GB"
+  type        = string
+  default     = "50"
+}
+
+variable "warm_storage_size" {
+  description = "Storage size of warm node in GB"
+  type        = string
+  default     = "0"
+}
+
+variable "cold_storage_size" {
+  description = "Storage size of cold node in GB"
+  type        = string
+  default     = "0"
+}
+
+variable "balancer_storage_class" {
+  description = "Storage class of balancer node in GB"
+  type        = string
+}
+
+variable "master_storage_class" {
+  description = "Storage class of master node in GB"
+  type        = string
+}
+
+variable "hot_storage_class" {
+  description = "Storage class of hot node in GB"
+  type        = string
+}
+
+variable "warm_storage_class" {
+  description = "Storage class of warm node in GB"
+  type        = string
+}
+
+variable "cold_storage_class" {
+  description = "Storage class of cold node in GB"
+  type        = string
+}
+
+variable "kibana_external_domain" {
+  description = "Kibana external domain"
+  type        = string
+}
