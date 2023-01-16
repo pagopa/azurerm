403f35462a65ce2d18503d3997fdd9741777d725
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-01-16 19:33:14 +0100

    fix version for internal modules

--- function_app/main.tf
+++ function_app/main.tf
@@ -1,6 +1,6 @@
 #tfsec:ignore:azure-storage-default-action-deny
 module "storage_account" {
-  source = "git::https://github.com/pagopa/azurerm.git//storage_account?ref=v2.7.0"
+  source = "git::https://github.com/pagopa/azurerm.git//storage_account?ref=version-unlocked"
 
   name                       = coalesce(var.storage_account_name, format("%sst", replace(var.name, "-", "")))
   account_kind               = var.storage_account_info.account_kind
@@ -18,7 +18,7 @@ module "storage_account" {
 module "storage_account_durable_function" {
   count = var.internal_storage.enable ? 1 : 0
 
-  source = "git::https://github.com/pagopa/azurerm.git//storage_account?ref=v2.7.0"
+  source = "git::https://github.com/pagopa/azurerm.git//storage_account?ref=version-unlocked"
 
   name                       = coalesce(var.storage_account_durable_name, format("%ssdt", replace(var.name, "-", "")))
   account_kind               = var.storage_account_info.account_kind
@@ -60,7 +60,7 @@ resource "azurerm_storage_container" "internal_container" {
 
 module "storage_account_durable_function_management_policy" {
   count  = length(local.internal_containers) == 0 ? 0 : 1
-  source = "git::https://github.com/pagopa/azurerm.git//storage_management_policy?ref=v3.6.1"
+  source = "git::https://github.com/pagopa/azurerm.git//storage_management_policy?ref=version-unlocked"
 
   storage_account_id = module.storage_account_durable_function[0].id
 
