0dd7cc926308e86207d306abb45257c43458df85
Marco Meretta <marco.meretta@gmail.com>
2023-02-17 10:09:29 +0100

    fix

--- elastic_stack/main.tf
+++ elastic_stack/main.tf
@@ -13,32 +13,32 @@ resource "kubernetes_manifest" "operator_manifest" {
   }
 }
 
-resource "kubernetes_manifest" "operator_manifest" {
-  depends_on = [
-    kubernetes_manifest.crds_manifest
-  ]
-  field_manager {
-    force_conflicts = true
-  }
-  computed_fields = ["metadata.labels", "metadata.annotations", "spec", "status"]
-  manifest = yamldecode(templatefile("${path.module}/yaml/elastic.yaml", {
-    num_node_balancer      = var.balancer_node_number
-    num_node_master        = var.master_node_number
-    num_node_hot           = var.hot_node_number
-    num_node_warm          = var.warm_node_number
-    num_node_cold          = var.cold_node_number
-    storage_size_balancer  = var.balancer_storage_size
-    storage_size_master    = var.master_storage_size
-    storage_size_hot       = var.hot_storage_size
-    storage_size_warm      = var.warm_storage_size
-    storage_size_cold      = var.cold_storage_size
-    storage_class_balancer = var.balancer_storage_class
-    storage_class_master   = var.master_storage_class
-    storage_class_hot      = var.hot_storage_class
-    storage_class_warm     = var.warm_storage_class
-    storage_class_cold     = var.cold_storage_class
-  }))
-}
+# resource "kubernetes_manifest" "operator_manifest" {
+#   depends_on = [
+#     kubernetes_manifest.crds_manifest
+#   ]
+#   field_manager {
+#     force_conflicts = true
+#   }
+#   computed_fields = ["metadata.labels", "metadata.annotations", "spec", "status"]
+#   manifest = yamldecode(templatefile("${path.module}/yaml/elastic.yaml", {
+#     num_node_balancer      = var.balancer_node_number
+#     num_node_master        = var.master_node_number
+#     num_node_hot           = var.hot_node_number
+#     num_node_warm          = var.warm_node_number
+#     num_node_cold          = var.cold_node_number
+#     storage_size_balancer  = var.balancer_storage_size
+#     storage_size_master    = var.master_storage_size
+#     storage_size_hot       = var.hot_storage_size
+#     storage_size_warm      = var.warm_storage_size
+#     storage_size_cold      = var.cold_storage_size
+#     storage_class_balancer = var.balancer_storage_class
+#     storage_class_master   = var.master_storage_class
+#     storage_class_hot      = var.hot_storage_class
+#     storage_class_warm     = var.warm_storage_class
+#     storage_class_cold     = var.cold_storage_class
+#   }))
+# }
 
 # resource "kubernetes_manifest" "kibana_manifest" {
 #   field_manager {
