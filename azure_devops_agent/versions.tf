e9190f62f024590205c69fc48cadbe5d174b01aa
mamari90 <130982006+mamari90@users.noreply.github.com>
2023-06-05 11:36:16 +0200

    fix version

--- azure_devops_agent/versions.tf
+++ azure_devops_agent/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.30.0, <= 3.53.0"
+      version = ">= 2.80.0, <= 2.99.0"
     }
     null = {
       source  = "hashicorp/null"
